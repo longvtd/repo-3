b5aba068568d1081f75db0cec29c4bd065a2bae0
longnguyen040201 <longnguyen040201@gmail.com>
2026-09-24 10:08:09 +0700

    first commit

---
+++ src/main/java/com/example/project/services/UserService.java
@@ -0,0 +1,60 @@
+package com.example.project.services;
+
+import java.util.List;
+
+import com.example.project.services.exceptions.DatabaseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.project.entities.User;
+import com.example.project.repositories.UserRepository;
+import com.example.project.services.exceptions.ResourceNotFoundException;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+       return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+}
